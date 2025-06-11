1b3d8d0520d3b84a6931f955e4c8040bbf1aa5c1
tarvel <taiyebabatundejames@gmail.com>
2025-06-11 01:08:55 +0100

    update

--- drfecommerce/product/views.py
+++ drfecommerce/product/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 
-# Create your views here.
+from .models import Category
+from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
+
+
+class CategoryViewSet(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing all categories
+    """
+
+    queryset = Category.objects.all()
+
+    @extend_schema(responses=CategorySerializer)
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class BrandViewSet(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing all brand
+    """
+
+    queryset = Category.objects.all()
+
+    @extend_schema(responses=BrandSerializer)
+    def list(self, request):
+        serializer = BrandSerializer(self.queryset, many=True)
+        return Response(serializer.data)
--- drfecommerce/settings/base.py
+++ drfecommerce/settings/base.py
@@ -20,6 +20,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # External_packages
+    "rest_framework",
+    "drf_spectacular",
+    # Internal_packages
     "drfecommerce.product",
 ]
 
@@ -82,3 +86,11 @@ STATIC_URL = "static/"
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Django DRF Ecommerce",
+}
